81d62df9032d9762d9240adad23e8b8eb864ba2a
Andrea To Chip <andreatochip@yahoo.com>
2025-10-25 15:09:32 +0800

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -4,4 +4,22 @@ function appendToDisplay(input) {
   display.value += input;
 }
 
-function clear
+function clearEntry() {
+  display.value = "";
+}
+
+function allClear() {
+  display.value = "";
+  a.value = "";
+  b.value = "";
+  operator.value = "";
+}
+
+function operate() {
+  try {
+    // display.value = eval(display.value);
+  }
+  catch(error) {
+    display.value = "Error";
+  }
+}
