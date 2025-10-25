84d3a2a267e1ed8987f76d30f7c80c428fbaa45b
Andrea To Chip <andreatochip@yahoo.com>
2025-10-25 15:31:11 +0800

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,13 @@
 const display = document.getElementById("display");
+const clear = document.querySelector(".clear");
+const equal = document.querySelector(".equal");
+const decimal = document.querySelector(".decimal");
+
+let numbers = document.querySelectorAll(".number");
+let oeprators = document.querySelectorAll(".operator-btn");
+
+let previousScreen = document.querySelector(".previous");
+let currentScreen = document.querySelector(".current");
 
 function appendToDisplay(input) {
   display.value += input;
